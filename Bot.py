5ce801871a5dd152242ed2d5a4fbdee081dbd2be
sfah007 <69042482+sfah007@users.noreply.github.com>
2022-03-31 06:00:49 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,6 +1,7 @@
 import telebot,random,os,requests
 from telebot import types
-bot = telebot.TeleBot('#Your Telegram Bot Token!')
+ttoken = os.environ['TOKEN']
+bot = telebot.TeleBot(ttoken)
 
 # Github: https://github.com/PluginX
 # Telegram: https://t.me/Plugin
@@ -14,7 +15,7 @@ def send_welcome(message):
 
         data = requests.get('https://apis.red/botdata/data.txt').text
 
-        if str(message.chat.id) in data:
+        if str(message.chat.id) == 1643689827 :
 
             key = types.InlineKeyboardMarkup()
             b1 = types.InlineKeyboardButton(text='Channel', url='https://t.me/CodeLeak')
@@ -45,7 +46,7 @@ def Get(message):
     try:
         data = requests.get('https://apis.red/botdata/data.txt').text
 
-        if str(message.chat.id) in data:
+        if str(message.chat.id) == 1643689827 :
             if msg.startswith('/pip'):
                 try:
 
